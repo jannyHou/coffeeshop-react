255e6f59bc2ead4520c374fc6e03638f32e9a6e8
Janny Hou <juehou2015@gmail.com>
2025-10-22 09:35:04 -0400

    feat: signup css improvement

--- coffeeshop/src/components/Footer.jsx
+++ coffeeshop/src/components/Footer.jsx
@@ -2,6 +2,6 @@ import "./Footer.css"
 
 export const Footer = () => {
   return (
-    <h4 className="footer">Copyright coffeelover@demo.com</h4>
+    <p className="footer">Copyright coffeelover@demo.com</p>
   )
 }
